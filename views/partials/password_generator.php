ea3a606bbe4d977236d82cbf33f14eddc142c3f3
L-cir94 <luca.cirigliano94@hotmail.it>
2023-05-09 15:47:32 +0200

    connect passwordlenght to function

--- views/partials/password_generator.php
+++ views/partials/password_generator.php
@@ -25,30 +25,26 @@ function random_string($length)
    $string = "";
    $chars = "abcdefghijklmanopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
    $size = strlen($chars);
-   for ($i = 0; $i < $length; $i++) {
+   for ($i = 0; $i < $_GET['passwordLength']; $i++) {
        $string .= $chars[rand(0, $size - 1)];
+
    }
    return $string; 
 }
 
-$password = random_string($value)
+$password = random_string($string)
 ?>
 <section class="newsletter bg-dark text-light py-5">
         <div class="container">
             <h3>Generate your password HERE</h3>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia possimus, explicabo amet quisquam laborum delectus quam iusto nemo consequuntur sint nulla quod sit aut animi molestiae corrupti atque? Eius, eveniet!
             </p>
-<!--             <?php if (!empty($_GET['email'])) : ?>
-                <div class="alert alert-<?= $alert['status']; ?>" role="alert">
-                    <strong><?= $alert['message']; ?></strong>
-                </div>
-            <?php endif; ?> -->
            
             <form action="" method="get">
                 <div class="d-flex justify-content-between align-items-center">
                     <p class="mb-0">Lunghezza Password:</p>
                     <div>
-                        <input type="number" min="8" max="20" required name="passwordLength" id="passwordLength" value="">
+                        <input type="number" min="8" max="50" required name="passwordLength" id="passwordLength" >
                         <label for="passwordLength"></label>
                     </div>
                 </div>
@@ -56,13 +52,13 @@ $password = random_string($value)
                     <p>Consenti ripetizioni di uno o più caratteri:</p>
                     <div class="d-flex flex-column text-start">
                         <div class="mb-3">
-                            <input type="radio" id="repetition" name="repetition" value="1">
+                            <input type="radio" id="repetition" name="repetition">
                             <label for="Si">Si</label><br>
-                            <input type="radio" id="repetition" name="repetition" value="0">
+                            <input type="radio" id="repetition" name="repetition">
                             <label for="No">No</label><br><br>                    
                             <input type="checkbox" id="Letters" name="Letters">
                             <label for="letters"> Lettere</label><br>
-                            <input type="checkbox" id="Numbers" name="Numbers">
+                            <input type="checkbox" id="Numbers" name="Numbers" value="1">
                             <label for="numbers">Numeri</label><br>
                             <input type="checkbox" id="Simbols" name="Simbols">
                             <label for="simbols"> Simboli</label><br>
